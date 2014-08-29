24b89a41b4dbf0ffa27b2ca8c9659f2ef108c3ee
joemakev <joemakev@gmail.com>
2014-08-29 18:44:52 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Brackets Backup
+
+This extension will create a backup copy (.bak) of the active file in its current directory.
+
+* You can trigger the file backup by going to the menu bar and clicking the following:
+1. MANUAL BACKUP : [Backup] -> [Backup - CREATE]
+2. AUTO BACKUP (Every 3 mins.) : [Backup] -> [Backup - START Auto]
+
+Screenshots:
+![Screenshot](https://github.com/joemakev/....)
---
+++ main.js
@@ -0,0 +1,112 @@
+define(function (require, exports, module) {
+    "use strict";
+    
+    //Load bracket modules
+    var CommandManager  = brackets.getModule("command/CommandManager"),
+        Menus           = brackets.getModule("command/Menus"),
+        FileUtils       = brackets.getModule("file/FileUtils"),
+        DocumentManager = brackets.getModule("document/DocumentManager"),
+        FileSystem      = brackets.getModule("filesystem/FileSystem"),
+        File            = brackets.getModule("filesystem/File");
+    
+    //Main variables
+    var backupInterval = 'undefined',
+        backupTime = 180000 /*300000 MS = 5 mins., 180000 MS = 3mins.*/,
+        isAutoBackup = false;
+
+    /* --- Main Functions ---  */
+    function createFileBackup() {
+        console.log('createFileBackup()');
+        ////window.alert("createFileBackup()");
+        
+        //Clear auto backup job
+        if(isAutoBackup) clearInterval(backupInterval);
+        
+        //Get the current document
+        var currDoc = DocumentManager.getCurrentDocument();
+        //var currOpenDocsArr = DocumentManager.getAllOpenDocuments(); //<-- Works
+        //console.log(currDoc);
+        var currDocTxt = currDoc.getText();
+        //var currDocFullPath = FileUtils.getDirectoryPath(currDoc.file.fullPath);
+        //var currDocName = FileUtils.getBaseName(currDoc.file.fullPath);
+        //console.log(currDocDir);
+        //console.log(currDocName+'.bak');
+        
+        //Set path and file variables
+        var currDocFile = currDoc.file;
+        var currDocOrigPath = currDoc.file._path;
+        var currDocBakPath = currDocOrigPath+'.bak';
+        
+        //Change doc path to bak
+        currDoc.file._path = currDocBakPath;
+        
+        //Create/write the backup file
+        ////console.log(currDoc.file._path);
+        var options = {};
+        options.blind = true;
+        currDocFile.write(currDocTxt, options);
+        ////console.log(currDocTxt);
+    
+        //Change doc path to orig
+        currDoc.file._path = currDocOrigPath;
+        
+        //Restart the auto backup, if enabled
+        if(isAutoBackup) startAutoBackup();
+        
+        
+    } //end of createFileBackup()
+    
+    function startAutoBackup() {
+        console.log('startAutoBackup()');
+        if(!isAutoBackup) {
+            menu.removeMenuItem(START_AUTO_BACKUP);
+            menu.addMenuItem(STOP_AUTO_BACKUP);
+            isAutoBackup = true;
+        }
+        clearInterval(backupInterval);
+        backupInterval = window.setInterval(function() {
+            console.log('[autoBackup]');
+            createFileBackup();
+        }, backupTime);
+    }
+    
+    function stopAutoBackup() {
+        menu.addMenuItem(START_AUTO_BACKUP);
+        menu.removeMenuItem(STOP_AUTO_BACKUP);
+        isAutoBackup = false
+        console.log('stopAutoBackup()');
+        clearInterval(backupInterval);
+    }
+    
+    function dummyFunction() {
+        console.log('dummyFunction()');
+    }
+    
+    /* --- Register Commands --- */
+    var CREATE_FILE_BACKUP = "bracketsBackup.createFileBackup";
+    CommandManager.register("Backup - CREATE", CREATE_FILE_BACKUP, createFileBackup);
+    
+    var START_AUTO_BACKUP = "bracketsBackup.startAutoFileBackup";
+    CommandManager.register("Backup - START Auto", START_AUTO_BACKUP, startAutoBackup);
+    
+    var STOP_AUTO_BACKUP = "bracketsBackup.stopAutoBackup";
+    CommandManager.register("Backup - STOP Auto", STOP_AUTO_BACKUP, stopAutoBackup);
+    
+    //var DUMMY_FUNCTION = "bracketsBackup.dummyFunction";
+    //CommandManager.register("Backup - Dummy Function", DUMMY_FUNCTION, dummyFunction);
+    
+    /* --- Create Menu Item --- */
+    //var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+    //menu.addMenuDivider();
+    var menu = Menus.addMenu("Backup", 'Backup', Menus.AFTER, Menus.AppMenuBar.FILE_MENU);
+    //menu.addMenuItem(DUMMY_FUNCTION);
+    menu.addMenuItem(CREATE_FILE_BACKUP);
+    menu.addMenuItem(START_AUTO_BACKUP);
+    
+    
+    
+    //We could also add a key binding at the same time:
+    //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-H");
+    //(Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
+    
+});
